90188a6e8cf5d7f5efe162e36e8bd9366bb5d931
nguyentuan <dungtuann67@gmail.com>
2024-03-02 22:59:11 +0700

    match component

---
+++ src/components/Match.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="wrapper">
+    <div class="d-flex align-items-center">
+      <div class="logo">
+        <img :src="teamOne.logo" :alt="teamOne.name">
+      </div>
+      <div class="name">{{ teamOne.name }}</div>
+      <div class="vs">VS</div>
+      <div class="name">{{ teamTwo.name }}</div>
+      <div class="logo">
+        <img :src="teamTwo.logo" :alt="teamTwo.name">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  id: { type: [Number | String] },
+  teamOne: { type: Object, required: true },
+  teamTwo: { type: Object, required: true },
+  date: { type: [String, Date], required: true },
+  time: { type: String, required: true }
+})
+const { teamOne, teamTwo, date, time } = props;
+</script>
+
+<style scoped>
+.logo {
+  width: 55px;
+  height: 70px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #fff;
+  clip-path: polygon(50% 0, 100% 25%, 100% 75%, 50% 100%, 0 75%, 0 25%);
+  /* border-radius: 50%; */
+  overflow: hidden;
+}
+.logo img {
+  width: 40px;
+  height: 40px;
+  object-fit: contain;
+}
+.name {
+  background-color: #000;
+  color: #fff;
+  font-weight: 500;
+  padding: 8px;
+  width: 220px;
+  text-align: center;
+  overflow: hidden;
+  height: 36px;
+  line-height: 1;
+}
+.vs {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-weight: 900;
+  font-size: 24px;
+  width: 60px;
+  height: 70px;
+  background-color: #091F5A;
+  color: #fff;
+  clip-path: polygon(50% 0, 100% 25%, 100% 75%, 50% 100%, 0 75%, 0 25%);
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,44 @@
 <template>
   <div class="container">
     <h1 class="fs-3 text-center text-white mt-4">Lượt trận hôm nay</h1>
+    <div class="mt-2 fs-3 text-center text-white">{{ todayString }}</div>
+    <div class="match-schedule mt-5">
+      <Match v-for="match in matchSchedule" :key="match.id" 
+        :teamOne="match.teamOne" :teamTwo="match.teamTwo" :date="match.date" :time="match.time"
+      />
+    </div>
   </div>
 </template>
 
 <script setup>
-
+import Match from '../components/Match.vue';
+const today = new Date();
+const todayString = today.getDate() + "/" + (Number(today.getMonth()) + 1) + "/"+today.getFullYear()
+const matchSchedule = [
+  {
+    id: "1",
+    teamOne: {
+      id: "1",
+      name: "Nguyễn Dũng Tuấn",
+      coach: "Carlo AnceloTuan",
+      logo: "https://upload.wikimedia.org/wikipedia/en/thumb/5/56/Real_Madrid_CF.svg/1200px-Real_Madrid_CF.svg.png"
+    },
+    teamTwo: {
+      id: "2",
+      name: "Trịnh Văn Quyết",
+      coach: "Erik ten Quyet",
+      logo: "https://static.bongda24h.vn/medias/standard/2021/11/6/213700-5.jpg"
+    },
+    date: today,
+    time: "12:15"
+  }
+]
 </script>
 
 <style scoped>
-
+.match-schedule {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
 </style>
